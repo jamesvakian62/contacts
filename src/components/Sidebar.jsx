72bf8a509bca185e2c1bdbb7e69a254ac4257645
James Avakian <95377031+jamesvakian62@users.noreply.github.com>
2021-12-15 23:05:42 -0800

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+function Sidebar() {
+    return (
+        <div className='Sidebar'>
+            <a href="./components/home">Home</a>
+            <a href="./components/family">Family</a>
+            <a href="./components/friends">Friends</a>
+            <a href="./components/coworkers">Coworkers</a>
+          
+        </div>
+    )
+}
+
+export default Sidebar
+
+
